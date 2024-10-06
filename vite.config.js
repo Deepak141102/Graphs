afb0a143cf079b271fa1c7ef504f04e6e225614f
Deepakgit config --global user.email d84255411@gamil.comgit config --global user.name Deepak <d84255411@gamil.com>
2024-10-06 11:09:16 +0530

    Updated for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite';  // Correct import for defineConfig
+import react from '@vitejs/plugin-react-swc';
+
 export default defineConfig({
   base: '/Graphs/', // Ensure this matches your repository name
-  plugins: [React()],
+  plugins: [react()],
 });
